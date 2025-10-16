5c9e8c71c814f665af5ed0f0857affff1ecc272f
asdf1234gyc <1483875459@qq.com>
2025-10-16 20:21:57 +0800

    Add testSubtraction

--- src/test/java/org/example/CalcTest.java
+++ src/test/java/org/example/CalcTest.java
@@ -5,10 +5,13 @@ import org.junit.jupiter.api.Test;
 
 public class CalcTest {
   Calc c = new Calc();
+  Calc d=new Calc();
 
     @Test
     void testAddition() {
         assertEquals(4, c.add(2,2));
     }
+    @Test
+    void testSubtraction(){assertEquals(2, d.subtract(4,2));}
 
 }
